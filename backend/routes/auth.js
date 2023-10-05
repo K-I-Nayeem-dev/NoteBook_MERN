fa947fea464b1161e6a9e8120174ef0507cdce49
Kamrul Islam Nayeem <naeem8541@gmail.com>
2023-10-06 03:17:46 +0600

    just update iNotebook

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,6 +3,8 @@ const User = require('../models/User');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const jsw_Screct = 'nayeemisCodingNow';
 
 // Create A New User Process/ Methos : Post '/auth/createUser' // No Login Required/ Start
 
@@ -24,14 +26,16 @@ router.post('/createUser',[
 
     try {
          //find email in data , is email found return true
-        let user = await User.findOne({email: req.body.email})
+        let user = await User.findOne({email: req.body.email});
 
         if(user){
             return res.status(400).json('Please Enter a Valid Email');
         }
 
+        // Set Password Plain to Hash & Also Salt
+
         const salt = await bcrypt.genSalt(10);
-        const secPass = await bcrypt.hash(req.body.password, salt)
+        const secPass = await bcrypt.hash(req.body.password, salt);
 
 
         //Insert date what gives in Database Start//
@@ -39,9 +43,20 @@ router.post('/createUser',[
             name: req.body.name,
             email: req.body.email,
             password: secPass
-        })
+        });
 
-        res.json(user)
+        //sending User Token
+
+        const data = {
+            user:{
+                id : user.id,
+            }
+        }
+        // user Authentication Json web Token
+        const authtoken = jwt.sign(data, jsw_Screct);
+        res.json({authtoken});
+
+
     } catch (error) {
         return res.status(500).json('Some Error Occurs!');
     }
@@ -52,8 +67,68 @@ router.post('/createUser',[
     // })
     
     //Insert date what gives in Database End//
+})
+
+
+//  Authentication a User Process/ Methos : Post '/auth/login' // No Login Required/ Start
+
+router.post('/login',[
+    body('email','Plz Enter a valid Email').isEmail(),
+    body('password','Password field can not be blank').exists()
+], async (req, res)=>{
+
+    // Error Send to Server Start
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    //destructuring object to check email and password
+
+    const {email, password} = req.body;
+
+    try {
+        // searching if email exists or not
+        let user = await User.findOne({email});
+        // let passwordCompare = await User.findOne({password});
+
+        //if password do not convert to hashed just copy & paste this code into if(!user) " && !passwordCompare "
+
+        // check email and if email not found return a bad status 
+        if(!user){
+            res.status(400).json({error: 'please check your credentials and try again'});
+        }
+
+
+        // check Password and if Password not found return a bad status 
+        let passwordCompare = await bcrypt.compare(password, user.password);
+
+        if(!passwordCompare){
+            res.status(400).json({error: 'please check your credentials and try again'});
+        }
+
+        // Sending jsonwebtoken with id to user 
+
+       //sending User Token
+
+        const data = {
+            user:{
+                id : user.id,
+            }
+        }
+        // user Authentication Json web Token
+        const authtoken = jwt.sign(data, jsw_Screct);
+        
+        res.json({authtoken});
+        
+    } catch (error) {
+        return res.status(500).json({error: 'Some Error Occurs!'});
+    }
+    
 
 })
+
+//  Authentication a User Process/ Methos : Post '/auth/login' // No Login Required/ End
+
 
 //export router 
 
